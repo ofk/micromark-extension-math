3f323819c93175808f085136b86c094763a011fb
ofk <ofkjpn+github@gmail.com>
2024-06-22 21:58:23 +0900

    Release

---
+++ lib/math-tex-text.d.ts
@@ -0,0 +1,9 @@
+/** @type {Construct} */
+export const mathTexText: Construct;
+export type Construct = import("micromark-util-types").Construct;
+export type TokenizeContext = import("micromark-util-types").TokenizeContext;
+export type Tokenizer = import("micromark-util-types").Tokenizer;
+export type Previous = import("micromark-util-types").Previous;
+export type Resolver = import("micromark-util-types").Resolver;
+export type State = import("micromark-util-types").State;
+export type Token = import("micromark-util-types").Token;
